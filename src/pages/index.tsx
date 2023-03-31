ca189d2bbc4d57fd1cb6359a6459d7bfbc8c4d0b
JasonBoyett <jason.boyett@gmail.com>
2023-03-31 01:00:26 -0500

    fixed spelling error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Signe up→</h3>
+              <h3 className="text-2xl font-bold">Sign up→</h3>
               <div className="text-lg">
                 Submit your information to enter our beta program.
                 Join us in creating a routeen to enhance student&#39;s reading speed and academic potential.
