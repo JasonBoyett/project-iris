0e09fdf78cf478848b1126d2ce314c113a7fac7f
JasonBoyett <jason.boyett@gmail.com>
2023-04-02 03:34:16 -0500

    schulte table is playable but sessions are not yet logged

--- src/pages/schulte.tsx
+++ src/pages/schulte.tsx
@@ -6,8 +6,11 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Page: NextPage = () => {
-    
+    let [currentNumber, setCurrentNumber] = useState(1);
+    let localNumber = currentNumber;  
     const [sideLength, setSideLength] = useState(7);
+    let [currentErrors, setCurrentErrors] = useState(0);
+    
 
     function shuffledNumbers(last: number): number[] {
     const arr: number[] | undefined[] = [];
@@ -22,10 +25,23 @@ const Page: NextPage = () => {
     }
 
     const [numbers, changeNumbers] = useState(shuffledNumbers(sideLength * sideLength));
+    
+    const handleClick = (content: number) => { 
+        if (content === localNumber || content === currentNumber){
+            setCurrentNumber(prevNumber => prevNumber + 1);
+            localNumber++;
+            console.log('compared', currentNumber);
+        }
+        else{
+            setCurrentErrors(prevErrors => prevErrors + 1);
+            console.log(content,currentNumber);
+        }
+    };
 
-    const Cell = (content: number | string): JSX.Element => {
-        return(
-            <button className='h-20 w-20 flex items-center justify-center border border-zinc-800 border-2 rounded hover:bg-gray-900'>
+    const Cell = (content: number): JSX.Element => {
+        let className = 'h-20 w-20 flex items-center justify-center border border-zinc-800 border-2 rounded bg-gray-900';
+    return(
+            <button className={className} onClick={() => handleClick(content)}>
                 <div className="text-center text-2xl text-white">{content}</div>
             </button>
         );
@@ -47,6 +63,11 @@ const Page: NextPage = () => {
     };
     const [table, setTable] = useState(generateTable());
     useEffect(() => { setTable(table); }, [table]);
+    useEffect(() => {
+        setCurrentNumber(localNumber)
+        console.log('triggered')
+    }, 
+    [currentNumber, currentErrors, localNumber]);
 
     return(
         <>
@@ -54,6 +75,10 @@ const Page: NextPage = () => {
                 <title>Schulte Table</title>
                 <meta name="Schulte Table" content="A speed reading game called Schulte Table" />
             </Head>
+            <div className='text-3xl text-white'>
+                <p>Errors: {currentErrors}</p>
+                <p>Number: {currentNumber}</p>
+            </div>
             <div className="flex justify-center h-screen items-center">
                 {table}
             </div>
