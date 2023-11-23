a12543a4a8476624409963cd3c22c02cbfd1a5ca
Jason Boyett <Jason.Boyett@gmail.com>
2023-11-23 09:38:31 -0600

    "speed test now scrolls through the passage."

--- src/pages/exercises/speedtest/index.tsx
+++ src/pages/exercises/speedtest/index.tsx
@@ -8,18 +8,22 @@ import { api } from '~/utils/api'
 import Head from 'next/head'
 import { FontProvider } from '~/cva/fontProvider'
 import Sidebar from '~/components/sidebar'
+import { userHilightToHex } from '~/utils/helpers'
 
 export const TESTS_PER_DAY = 10
 
 export default function Page() {
   const [counter, setCounter] = useState(0)
+  const [hex, setHex] = useState<string>('#000000')
   const [words, setWords] = useState<string[]>([''])
   const [font, setFont] = useState<Font>('sans')
   const userStore = useUserStore((state) => state)
   const exerciseStore = useSpeedTestStore((state) => state)
   const [loading, setLoading] = useState(true)
   const [time, setTime] = useState(100)
-  const [display, setDisplay] = useState('Loading...')
+  const [previous, setPrevious] = useState<string>('')
+  const [next, setNext] = useState<string>('')
+  const [current, setCurrent] = useState('Loading...')
   const { data } = api.getExcerciseProps.getSingleSpeedTestProps.useQuery()
   const router = useRouter()
 
@@ -29,7 +33,7 @@ export default function Page() {
       .catch((err) => console.log(err))
   }
 
-  const wpmToMiliseconds = (time: number) => {
+  function wpmToMiliseconds(time: number) {
     return 60_000 / time
   }
 
@@ -43,6 +47,7 @@ export default function Page() {
       return data.passage.split(' ')
     })
     setFont(userStore.user.font)
+    setHex(userHilightToHex(userStore.user))
     exerciseStore.setUp(data)
     setLoading(() => false)
     setTime(wpmToMiliseconds(userStore.user.testSpeed))
@@ -51,9 +56,17 @@ export default function Page() {
   useInterval(() => {
     if (!!loading) return
     if (counter === words.length) return navigateToQuestion()
-    setDisplay((): string => {
+    setPrevious((): string => {
+      if (!words.at(counter - 1)) return ''
+      return words.at(counter - 1) as string
+    })
+    setCurrent((): string => {
       if (!words.at(counter)) return ''
       return words.at(counter) as string
+    })
+    setNext((): string => {
+      if (!words.at(counter + 1)) return ''
+      return words.at(counter + 1) as string
     })
     setCounter((prev) => prev + 1)
   }, time)
@@ -64,12 +77,20 @@ export default function Page() {
       <Sidebar />
 
       <main>
-        <FontProvider
-          className='h-screen flex items-center justify-center'
-          font={font}
-        >
-          <h1 className='text-4xl text-center text-white'>{display}</h1>
-        </FontProvider>
+        <center>
+          <FontProvider
+            className='h-screen flex items-center justify-center'
+            font={font}
+          >
+            <div
+              className='flex flex-col gap-4 items-center md:w-2/3 w-5/6 bg-white rounded-xl justify-center p-4'
+            >
+              <h1 className='text-4xl text-center'>{previous}</h1>
+              <h1 className={ `text-4xl text-center bg-[${ hex }] md:w-1/3 w-5/6 rounded-xl` }>{current}</h1>
+              <h1 className='text-4xl text-center'>{next}</h1>
+            </div>
+          </FontProvider>
+        </center>
       </main>
     </>
   )
