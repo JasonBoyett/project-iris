8d8a91a9815faaeb308fb1f5f0a6acb79579c2fd
Jason Boyett <jason.boyett@gmail.com>
2025-10-27 15:08:27 -0500

    fixed cron jobs

--- apps/nextjs/src/pages/api/cron/reset-tested/index.ts
+++ apps/nextjs/src/pages/api/cron/reset-tested/index.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@acme/db/src/generated/client'
 
-export async function GET(request: NextRequest) {
-  // const authHeader = request.headers.get('authorization')
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
-  // }
+export default async function GET(request: NextRequest, response: any) {
+  // response is any here but it's a NextResponse. We're using any here
+  // because the status property is not being typed correctly
+  const authHeader = request.headers.get('authorization')
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return response.status(401).json({ error: 'Unauthorized' })
+  }
   try {
     const prisma = new PrismaClient()
     await prisma.user.updateMany({
@@ -13,18 +15,17 @@ export async function GET(request: NextRequest) {
         tested: false,
       },
     })
-    return NextResponse.json({
+    return response.status(200).json({
       message: 'Reset tested job completed successfully',
       timestamp: new Date().toISOString(),
     })
   } catch (error) {
     console.error('Reset tested job failed:', error)
-    return NextResponse.json(
-      { error: 'Reset tested job failed' },
-      { status: 500 },
-    )
+    return response.status(500).json({
+      error: `Reset tested job failed ${error}`,
+    })
   }
 }
 
-// Prevent caching
 export const dynamic = 'force-dynamic'
+// export const runtime = 'edge'
